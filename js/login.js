98348cf0f6edab551907c6546ac6efd8dd49a6b3
Bjonne <Bjonne696@gmail.com>
2023-10-01 14:24:07 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -52,43 +52,51 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // REGISTER
-    document.getElementById('registerForm').addEventListener('submit', async function(event) {
-        event.preventDefault();
 
-        const email = document.getElementById('registerEmail').value;
-        const username = document.getElementById('registerUsername').value;
-        const password = document.getElementById('registerPassword').value;
-        const confirmPassword = document.getElementById('registerConfirmPassword').value;
+// REGISTER
+document.getElementById('registerForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
 
-        if (password !== confirmPassword) {
-            displayErrorMessage('Passwords do not match!');
-            return;
+    const email = document.getElementById('registerEmail').value;
+    const username = email;  // Username is the same as the email used to register
+    const password = document.getElementById('registerPassword').value;
+    const confirmPassword = document.getElementById('registerConfirmPassword').value;
+
+    // Validate the email (which is now also the username)
+    if (!/^[a-zA-Z0-9_@.]+@stud\.noroff\.no$/.test(email)) {
+        displayErrorMessage('Please use your stud.noroff.no email address for registration.');
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        displayErrorMessage('Passwords do not match!');
+        return;
+    }
+
+    try {
+        let response = await fetch(`${API_URL}/auction/auth/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: username,
+                email: email,
+                password: password
+            })
+        });
+
+        if (response.ok) {
+            displaySuccessMessage('Successfully registered. You can now log in.');
+        } else {
+            const errorData = await response.json();
+            console.error(errorData);
+            displayErrorMessage('Error during registration. Please check your input.');
         }
+    } catch (error) {
+        console.error('There was an error during registration', error);
+        displayErrorMessage('Error during registration. Please try again.');
+    }
+});
 
-        try {
-            let response = await fetch(`${API_URL}/auction/auth/register`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    name: username,
-                    email: email,
-                    password: password
-                })
-            });
-
-            if (response.ok) {
-                displaySuccessMessage('Successfully registered. You can now log in.');
-            } else {
-                const errorText = await response.text();
-                console.error(errorText);
-                displayErrorMessage('Error during registration. Please try again.');
-            }
-        } catch (error) {
-            console.error('There was an error during registration', error);
-            displayErrorMessage('Error during registration. Please try again.');
-        }
-    });
 });
