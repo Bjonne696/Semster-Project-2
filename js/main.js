9ca6082ef499da6bc09d0ad9527cb0465d0b2b2f
Bjonne <Bjonne696@gmail.com>
2023-10-01 17:42:30 +0200

    uopdated userbanner

--- js/main.js
+++ js/main.js
@@ -34,3 +34,28 @@ document.addEventListener('DOMContentLoaded', function() {
         alert('Logged out successfully.');
     });
 });
+
+async function updateCredits() {
+    const userToken = localStorage.getItem('userToken');
+    if (!userToken) {
+        return;
+    }
+
+    // Adjust the URL if necessary
+    const response = await fetch('https://api.noroff.dev/api/v1/auction/profiles/my_username', {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + userToken
+        }
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        document.getElementById('credits').textContent = `${data.credits} Credits`;
+    } else {
+        console.error("Failed to fetch user data.");
+    }
+}
+
+// Call the function to populate credits on DOM load
+updateCredits();
