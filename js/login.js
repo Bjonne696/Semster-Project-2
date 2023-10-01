ec0196f890c59a3b9f8387e0f2913bbd65b4a1c3
Bjonne <Bjonne696@gmail.com>
2023-10-01 14:57:23 +0200

    update

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
+
     const API_URL = 'https://api.noroff.dev/api/v1';
 
     function displaySuccessMessage(message) {
@@ -14,11 +14,17 @@ document.addEventListener('DOMContentLoaded', function() {
         errorDiv.classList.remove('d-none');
     }
 
+    // Transform the email into a valid name format
+    const transformEmailToName = (email) => {
+        return email.replace(/[@.]/g, '');
+    };
+
     // LOGIN
     document.getElementById('loginForm').addEventListener('submit', async function(event) {
         event.preventDefault();
-
-        const username = document.getElementById('registerUsername').value; // Adjusted this from loginUsername to registerUsername
+    
+        const email = document.getElementById('loginEmail').value; // Ensure this ID matches the login form's email input ID
+        const username = transformEmailToName(email); // If the API expects a username, transform the email to a username format
         const password = document.getElementById('loginPassword').value;
 
         try {
@@ -36,11 +42,11 @@ document.addEventListener('DOMContentLoaded', function() {
             if (response.ok) {
                 let data = await response.json();
                 localStorage.setItem('userToken', data.token);
-                localStorage.setItem('credits', data.credits); // Store credits in localStorage
-                displaySuccessMessage("Logged in successfully!"); // display success
-                setTimeout(() => { 
-                    window.location.href = 'dashboard.html'; 
-                }, 2000); // redirect after 2 seconds
+                localStorage.setItem('credits', data.credits);
+                displaySuccessMessage("Logged in successfully!");
+                setTimeout(() => {
+                    window.location.href = 'dashboard.html';
+                }, 2000);
             } else {
                 const errorText = await response.text();
                 console.error(errorText);
@@ -52,51 +58,53 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // REGISTER
+    document.getElementById('registerForm').addEventListener('submit', async function(event) {
+        event.preventDefault();
 
-// REGISTER
-document.getElementById('registerForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
+        const email = document.getElementById('registerEmail').value;
+        const username = transformEmailToName(email);  // Use the transform function
+        const password = document.getElementById('registerPassword').value;
+        const confirmPassword = document.getElementById('registerConfirmPassword').value;
 
-    const email = document.getElementById('registerEmail').value;
-    const username = email;  // Username is the same as the email used to register
-    const password = document.getElementById('registerPassword').value;
-    const confirmPassword = document.getElementById('registerConfirmPassword').value;
-
-    // Validate the email (which is now also the username)
-    if (!/^[a-zA-Z0-9_@.]+@stud\.noroff\.no$/.test(email)) {
-        displayErrorMessage('Please use your stud.noroff.no email address for registration.');
-        return;
-    }
-
-    if (password !== confirmPassword) {
-        displayErrorMessage('Passwords do not match!');
-        return;
-    }
-
-    try {
-        let response = await fetch(`${API_URL}/auction/auth/register`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name: username,
-                email: email,
-                password: password
-            })
-        });
-
-        if (response.ok) {
-            displaySuccessMessage('Successfully registered. You can now log in.');
-        } else {
-            const errorData = await response.json();
-            console.error(errorData);
-            displayErrorMessage('Error during registration. Please check your input.');
+        if (!/^[a-zA-Z0-9_.]+@stud.noroff.no$/.test(email)) {
+            displayErrorMessage('Please use your stud.noroff.no email address for registration.');
+            return;
         }
-    } catch (error) {
-        console.error('There was an error during registration', error);
-        displayErrorMessage('Error during registration. Please try again.');
-    }
-});
 
+        if (!/^[a-zA-Z0-9_]{1,20}$/.test(username)) {
+            displayErrorMessage('Username can only use a-Z, 0-9, and _. Max 20 characters.');
+            return;
+        }
+
+        if (password !== confirmPassword) {
+            displayErrorMessage('Passwords do not match!');
+            return;
+        }
+
+        try {
+            let response = await fetch(`${API_URL}/auction/auth/register`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: username,
+                    email: email,
+                    password: password
+                })
+            });
+
+            if (response.ok) {
+                displaySuccessMessage('Successfully registered. You can now log in.');
+            } else {
+                const errorData = await response.json();
+                console.error(errorData);
+                displayErrorMessage('Error during registration. Please check your input.');
+            }
+        } catch (error) {
+            console.error('There was an error during registration', error);
+            displayErrorMessage('Error during registration. Please try again.');
+        }
+    });
 });
